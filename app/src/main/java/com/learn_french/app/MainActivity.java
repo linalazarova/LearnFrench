6e672031fe992d28ca17c9583c6de04e45aeaa52
Lina Lazarova <78424163+linalazarova@users.noreply.github.com>
2021-05-27 19:28:47 +0300

    Added Activities

--- app/src/main/java/com/learn_french/app/MainActivity.java
+++ app/src/main/java/com/learn_french/app/MainActivity.java
@@ -2,13 +2,97 @@ package com.learn_french.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.alphabet);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlphabetActivity();
+            }
+        });
+
+        button = findViewById(R.id.color);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorsActivity();
+            }
+        });
+
+        button = findViewById(R.id.numbers);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNumbersActivity();
+            }
+        });
+
+        button = findViewById(R.id.days);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDaysActivity();
+            }
+        });
+
+        button = findViewById(R.id.months);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMonthsActivity();
+            }
+        });
+
+        button = findViewById(R.id.phrases);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPhrasesActivity();
+            }
+        });
+    }
+
+    public void openAlphabetActivity(){
+        Intent intent = new Intent(this, AlphabetActivity.class);
+        startActivity(intent);
+    }
+
+    public void openColorsActivity(){
+        Intent intent = new Intent(this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openNumbersActivity(){
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+
+    public void openDaysActivity(){
+        Intent intent = new Intent(this, DaysActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMonthsActivity(){
+        Intent intent = new Intent(this, MonthsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPhrasesActivity(){
+        Intent intent = new Intent(this, PhrasesActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/learn_french/app/MonthsActivity.java
@@ -0,0 +1,14 @@
+package com.learn_french.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonthsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_months);
+    }
+}
