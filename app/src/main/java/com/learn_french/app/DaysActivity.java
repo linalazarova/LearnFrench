10f22ad0bca50423e37e3f688584b699a9564f67
Lina Lazarova <78424163+linalazarova@users.noreply.github.com>
2021-05-28 12:20:43 +0300

    Edited DaysActivity and MonthsActivity, added images and audio files

--- app/src/main/java/com/learn_french/app/DaysActivity.java
+++ app/src/main/java/com/learn_french/app/DaysActivity.java
@@ -2,13 +2,88 @@ package com.learn_french.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DaysActivity extends AppCompatActivity {
+
+    private ImageButton sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_days);
+
+        sound = findViewById(R.id.mondaybtn);
+        final MediaPlayer mpMonday = MediaPlayer.create(this, R.raw.lundi);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpMonday.start();
+            }
+        });
+
+        sound = findViewById(R.id.tuesdaybtn);
+        final MediaPlayer mpTuesday = MediaPlayer.create(this, R.raw.mardi);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpTuesday.start();
+            }
+        });
+
+        sound = findViewById(R.id.wednesdaybtn);
+        final MediaPlayer mpWednesday = MediaPlayer.create(this, R.raw.mercredi);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpWednesday.start();
+            }
+        });
+
+        sound = findViewById(R.id.thursdaybtn);
+        final MediaPlayer mpThursday = MediaPlayer.create(this, R.raw.jeudi);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpThursday.start();
+            }
+        });
+
+        sound = findViewById(R.id.fridaybtn);
+        final MediaPlayer mpFriday = MediaPlayer.create(this, R.raw.vendredi);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpFriday.start();
+            }
+        });
+
+        sound = findViewById(R.id.saturdaybtn);
+        final MediaPlayer mpSaturday = MediaPlayer.create(this, R.raw.samedi);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpSaturday.start();
+            }
+        });
+
+        sound = findViewById(R.id.sundaybtn);
+        final MediaPlayer mpSunday = MediaPlayer.create(this, R.raw.dimanche);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpSunday.start();
+            }
+        });
     }
 }
--- app/src/main/java/com/learn_french/app/MonthsActivity.java
+++ app/src/main/java/com/learn_french/app/MonthsActivity.java
@@ -2,13 +2,139 @@ package com.learn_french.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MonthsActivity extends AppCompatActivity {
+
+    private ImageButton sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_months);
+
+        sound = findViewById(R.id.januarybtn);
+        final MediaPlayer mpJanuary = MediaPlayer.create(this, R.raw.janvier);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpJanuary.start();
+            }
+        });
+
+        sound = findViewById(R.id.februarybtn);
+        final MediaPlayer mpFebruary = MediaPlayer.create(this, R.raw.fevrier);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpFebruary.start();
+            }
+        });
+
+        sound = findViewById(R.id.marchbtn);
+        final MediaPlayer mpMarch = MediaPlayer.create(this, R.raw.mars);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpMarch.start();
+            }
+        });
+
+        sound = findViewById(R.id.aprilbtn);
+        final MediaPlayer mpApril = MediaPlayer.create(this, R.raw.avril);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpApril.start();
+            }
+        });
+
+        sound = findViewById(R.id.maybtn);
+        final MediaPlayer mpMay = MediaPlayer.create(this, R.raw.mai);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpMay.start();
+            }
+        });
+
+        sound = findViewById(R.id.junebtn);
+        final MediaPlayer mpJune = MediaPlayer.create(this, R.raw.juin);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpJune.start();
+            }
+        });
+
+        sound = findViewById(R.id.julybtn);
+        final MediaPlayer mpJuly = MediaPlayer.create(this, R.raw.juillet);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpJuly.start();
+            }
+        });
+
+        sound = findViewById(R.id.augustbtn);
+        final MediaPlayer mpAugust = MediaPlayer.create(this, R.raw.aout);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpAugust.start();
+            }
+        });
+
+        sound = findViewById(R.id.septemberbtn);
+        final MediaPlayer mpSeptember = MediaPlayer.create(this, R.raw.septembre);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpSeptember.start();
+            }
+        });
+
+        sound = findViewById(R.id.octoberbtn);
+        final MediaPlayer mpOctober = MediaPlayer.create(this, R.raw.octobre);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpOctober.start();
+            }
+        });
+
+        sound = findViewById(R.id.novemberbtn);
+        final MediaPlayer mpNovember = MediaPlayer.create(this, R.raw.novembre);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpNovember.start();
+            }
+        });
+
+        sound = findViewById(R.id.decemberbtn);
+        final MediaPlayer mpDecember = MediaPlayer.create(this, R.raw.decembre);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mpDecember.start();
+            }
+        });
+
     }
 }
